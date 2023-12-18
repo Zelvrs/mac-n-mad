1c14160dbaa1f900fcd92b7cbfb008afc2a70bef
gazel <gazel@ngoprekweb.com>
2023-12-18 22:42:53 +0700

    merubah page menu menjadi responsive dan membuat animasi saat scroll di page home, dan mencegah animasi saat preload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
-// .Toggle class active  
-const nav = document.querySelector('.nav')
-const hamburger = document.querySelector('#hamburger-menu')
+// -Toggle class active
+const nav = document.querySelector(".nav");
+const hamburger = document.querySelector("#hamburger-menu");
 
-// .ketika hamburger menu di klik
+// -ketika hamburger menu di klik
 hamburger.onclick = (e) => {
-    e.preventDefault();
-    nav.classList.toggle('active');
-}
+	e.preventDefault();
+	nav.classList.toggle("active");
+};
 
-// .klik diluar sidebar untuk menghilangkan nav active
-document.addEventListener('click' ,(e) =>{
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active')
-    }
-})
+// -klik diluar sidebar untuk menghilangkan nav active
+document.addEventListener("click", (e) => {
+	if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+		navbarNav.classList.remove("active");
+	}
+});
+
+// -animasi history
+const elHidden = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry);
+		if (entry.isIntersecting) {
+			entry.target.classList.add("show");
+		} else {
+			entry.target.classList.remove("show");
+		}
+	});
+});
+
+elHidden.forEach((el) => observer.observe(el));
+
+
+// -menghentikan animasi saat load pertama kali
+setTimeout(() => {
+	document.body.className = "";
+}, 500);
